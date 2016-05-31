00fc8a3e7694da288a6ed2b0f911b6b4660e866a
actifq <actifq@gmail.com>
2016-05-31 17:11:03 +0900

    찬재 >> data 패키지왜안올라감?

--- Fit/src/main/java/com/sist/fit/MainController.java
+++ Fit/src/main/java/com/sist/fit/MainController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.sist.main.data.TourDTO;
-import com.sist.main.data.TourManager;
-
+import com.sist.data.*;
 
 
 @Controller
