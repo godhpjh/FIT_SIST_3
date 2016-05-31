332588c6780d25b954b3c3ad5557dadc0a83ece9
sist <sist@localhost>
2016-05-31 16:45:05 +0900

    그래프보영

--- Fit/src/main/java/com/sist/fit/MainController.java
+++ Fit/src/main/java/com/sist/fit/MainController.java
@@ -1,13 +1,42 @@
 package com.sist.fit;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sist.main.data.TourDTO;
+import com.sist.main.data.TourManager;
+
+
 
 @Controller
 public class MainController {
-
+	
+	@Autowired
+	private TourManager tmgr;			//메인 첫번째 한국 여행 동향
+	
 	@RequestMapping("main.do")
-	public String main_page() {
+	public String main_page(Model model) throws Exception{
+		
+		List<TourDTO> tlist=tmgr.tourYearData();		//1.국내여행동향인원수
+		List<TourDTO> inoutlist=tmgr.tourInOutData();	//2.입국출국인원수
+		
+		for(TourDTO t:tlist){
+			System.out.println("국내여행동향:"+t.getOneday()+","+t.getSleep()+","+t.getTotal()+","+t.getYear());
+		}
+		
+		for(TourDTO t:inoutlist){
+			System.out.println("입국인원수:"+t.getSleep()+", 출국인원수:"+t.getTotal());
+		}
+		
+		model.addAttribute("tlist",tlist);
+		model.addAttribute("inoutlist",inoutlist);
+		
 		return "main";
 	}
 
